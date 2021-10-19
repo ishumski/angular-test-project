29d0059215050a68ac49bfce06a398ec6af41d44
Ilia Shumski <whalterwhite23@gmail.com>
2021-10-19 10:53:24 +0300

    Add game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,36 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{ type: 'server', name: 'LOL', content: 'UIIII' }]
+  // serverElements = [{ type: 'server', name: 'LOL', content: 'UIIII' }]
 
-  onServerAdded( serverData: { serverName: string; serverContent: string }) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    })
-    console.log(this.serverElements)
-  }
+  // onServerAdded( serverData: { serverName: string; serverContent: string }) {
+  //   this.serverElements.push({
+  //     type: 'server',
+  //     name: serverData.serverName,
+  //     content: serverData.serverContent
+  //   })
+  //   console.log(this.serverElements)
+  // }
 
-  onAddBlueprintAdded(blueprintData: { serverName: string; serverContent: string }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent
-    })
-    console.log(this.serverElements)
+  // onAddBlueprintAdded(blueprintData: { serverName: string; serverContent: string }) {
+  //   this.serverElements.push({
+  //     type: 'blueprint',
+  //     name: blueprintData.serverName,
+  //     content: blueprintData.serverContent
+  //   })
+  //   console.log(this.serverElements)
+  // }
+
+  oddNumbers: number[] = []
+  evenNumbers: number[] = []
+
+  onIntervalFired(firedNumber: number) {
+    console.log(firedNumber)
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber)
+    } else {
+      this.oddNumbers.push(firedNumber)
+      console.log(this.oddNumbers)
+    }
   }
 }
---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core'
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  @Output() intervalFired = new EventEmitter<number>()
+  interval: any
+  lastNumber: number = 0
+
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber++)
+    }, 1000)
+  }
+
+  onPauseGame() {
+    clearInterval(this.interval)
+  }
+
+  ngOnInit(): void {}
+}
